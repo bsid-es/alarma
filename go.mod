bsid-es/steven

Import current state

go.mod
@@ -1,0 +1,3 @@
+module bsid.es/steven
+
+go 1.18

bsid-es/alarma

go.mod
@@ -1,0 +1,3 @@
+module bsid.es/alarma
+
+go 1.18